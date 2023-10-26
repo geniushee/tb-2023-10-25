387f724e94d967c2fd9372bb4873d0beede57390
geniushee <h1116jeon@gmail.com>
2023-10-26 14:07:13 +0900

    4,5단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,10 +1,16 @@
 package com.ll;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     void run() {
         System.out.println("=== 명언 앱 ===");
+        List<Quote> quotations = new ArrayList<>();
+        int quotesid = 0;
+
         while (true) {
             System.out.print("명령) ");
 
@@ -12,13 +18,24 @@ public class App {
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
                 break;
-            } else if(cmd.equals("등록")){
-                System.out.println("명언 : ");
+            } else if (cmd.equals("등록")) {
+
+                System.out.print("명언 : ");
                 String content = scanner.nextLine();
-                System.out.println("작가 : ");
+                System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
 //                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
-                System.out.println("1번 명언이 등록되었습니다.");
+
+                quotesid++; // 등록시 번호 증가
+                quotations.add(new Quote(quotesid, content, authorName));
+
+                System.out.println(quotesid + "번 명언이 등록되었습니다.");
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("--------------------------");
+                for (Quote Q : quotations) {
+                    System.out.printf("%d / %s / %s\n", Q.no, Q.author, Q.content);
+                }
             }
 
 
---
+++ src/main/java/com/ll/Quote.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Quote {
+    int no;
+    String content;
+    String author;
+
+    public Quote(int Q_no, String Q_content, String Q_author) {
+        no = Q_no;
+        content = Q_content;
+        author = Q_author;
+    }
+}
