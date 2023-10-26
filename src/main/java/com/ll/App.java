d0b0c5db9ee160a9b76136454b84d2ee4a12f707
geniushee <h1116jeon@gmail.com>
2023-10-26 10:49:32 +0900

    2,3단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -12,7 +12,15 @@ public class App {
             String cmd = scanner.nextLine();
             if (cmd.equals("종료")) {
                 break;
+            } else if(cmd.equals("등록")){
+                System.out.println("명언 : ");
+                String content = scanner.nextLine();
+                System.out.println("작가 : ");
+                String authorName = scanner.nextLine();
+//                System.out.printf("명언 : %s, 작가 : %s\n", content, authorName);
+                System.out.println("1번 명언이 등록되었습니다.");
             }
+
 
             System.out.printf("입력하신 명령: %s\n", cmd);
         }
